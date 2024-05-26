b6948caf8e86640dd51dba4d4a422fc1795322c8
NadimGhaznavi <nghaznavi@gmail.com>
2024-05-26 12:48:34 -0400

    syntax

--- code/xmr-earnings.js
+++ code/xmr-earnings.js
@@ -1,39 +1,43 @@
 d3.csv("/data/xmr-earnings.csv", function(data) {
-  Array.from(data).forEach(function(d) {
-    d.Amount = +d.Amount;
-    d.Total = +d.Total;
-    console.log(data); // Log the data to verify its structure 
-  });
+  // Convert data to an array explicitly
+const dataArray = Array.from(data);
 
+  // Process data and create scales
+const x = d3.scaleTime()
+    .domain(d3.extent(dataArray.filter(d => d.Date && d.Total), d => new Date(d.Date)))
+    .range([0, 800]);
 
-  var chart = d3.select("#chart")
+  const y = d3.scaleLinear()
+    .domain(d3.extent(dataArray.filter(d => d.Date && d.Total), d => d.Total))
+    .range([0, 800]);
+
+  // Create x and y axes
+const xAxis = d3.axisBottom(x).tickFormat(d3.timeFormat("%m/%d"));
+  const yAxis = d3.axisLeft(y);
+
+  // Create line generator
+const line = d3.line()
+    .x(d => x(new Date(d.Date)))
+    .y(d => y(d.Total));
+
+  // Append SVG
+const svg = d3.select("#chart")
     .append("svg")
     .attr("width", 800)
     .attr("height", 800);
 
-  var x = d3.scaleTime()
-    .domain(d3.extent(data.filter(d => d.Date && d.Total), function(d) { return new Date(d.Date); }))
-    .range([0, 800]);
-
-  var y = d3.scaleLinear()
-    .domain(d3.extent(data.filter(d => d.Date && d.Total), function(d) { return d.Total; }))
-    .range([0, 800]);
-
-  var xAxis = d3.axisBottom(x).tickFormat(d3.timeFormat("%m/%d"));
-  var yAxis = d3.axisLeft(y);
-  chart.append("g")
+  // Append x and y axes
+svg.append("g")
     .attr("transform", "translate(0," + 400 + ")")
     .call(xAxis);
-  chart.append("g")
+
+  svg.append("g")
     .attr("transform", "translate(0,0)")
     .call(yAxis);
 
-  var line = d3.line()
-    .x(function(d) { return x(new Date(d.Date)); })
-    .y(function(d) { return y(d.Total); });
-
-  chart.append("path")
-    .datum(data)
+  // Append line
+svg.append("path")
+    .datum(dataArray)
     .attr("fill", "none")
     .attr("stroke", "steelblue")
     .attr("stroke-width", 1.5)
