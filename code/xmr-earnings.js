a01fbcb33d601b3f7b5d61444eaa86b29857457c
NadimGhaznavi <nghaznavi@gmail.com>
2024-05-27 10:15:41 -0400

    Apex + D3

--- code/xmr-earnings.js
+++ code/xmr-earnings.js
@@ -1,47 +1,48 @@
-d3.csv("/data/xmr-earnings.csv", function(data) {
-  // Convert data to X and R arrays for ApexCharts
-const xData = data.map(d => d.xValue); // Replace 'xValue' with your X data field name
-const rData = [];
-  for (let i = 0; i < data.length - 1; i++) {
-    rData.push(data[i + 1].xValue - data[i].xValue);
+document.addEventListener('DOMContentLoaded', function(
+) {
+  var options = {
+    chart: {
+      type: 'line'
+},
+    series: [{
+      name: 'X',
+      data: [] // Will be populated with X data
+}, {
+      name: 'R',
+      data: [] // Will be populated with R data
+}],
+    xaxis: {
+      categories: [] // Will be populated with your custom categories
+}
   }
 
-  // Now create the ApexCharts XmR chart
-let chart = new ApexCharts(document.querySelector("#chart"), {
-    series: [{
+  var chart = new ApexCharts(document.querySelector("#chart"), options);
+
+  chart.render();
+
+  d3.csv("/data/xmr-earnings.csv", function(data) {
+    // Convert data to X and R arrays
+const xData = data.map(d => d.Date);
+    const rData = [];
+    for (let i = 0; i < data.length - 1; i++) {
+      rData.push(data[i + 1].Total - data[i].Total);
+    }
+
+    // Extract categories from CSV data
+const categories = data.map(d => d.Date);
+
+    // Update the chart series data and redraw the chart
+chart.updateSeries([{
       name: 'X',
       data: xData
     }, {
       name: 'R',
       data: rData
-    }],
-    chart: {
-      type: 'line',
-      height: 350
-},
-    plotOptions: {
-      line: {
-        color: ['#FF6600', '#00796B']
+    }]);
+    chart.updateOptions({
+      xaxis: {
+        categories: categories
       }
-    },
-    dataLabels: {
-      enabled: true,
-      color: '#000000'
-},
-    stroke: {
-      width: [2, 2],
-      curve: 'smooth'
-},
-    fill: {
-      opacity: [0, 0]
-    },
-    markers: {
-      size: 0
-},
-    xaxis: {
-      type: 'datetime'
-}
+    });
   });
-
-  chart.render();
 });
