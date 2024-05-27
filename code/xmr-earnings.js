3deca701026796fdba680efc0e875e773dcaf568
NadimGhaznavi <nghaznavi@gmail.com>
2024-05-26 21:52:14 -0400

    syntax

--- code/xmr-earnings.js
+++ code/xmr-earnings.js
@@ -1,26 +1,52 @@
-d3.csv("/data/xmr-earnings.csv").then(data => {
-  // data is an array of objects, each representing a row from the CSV file
+// set the dimensions and margins of the graph
+var margin = {top: 10, right: 30, bottom: 30, left: 60},
+    width = 460 - margin.left - margin.right,
+    height = 400 - margin.top - margin.bottom;
 
-const table = d3.select("#csv-data").append("table");
+// append the svg object to the body of the page
+var svg = d3.select("#csv-chart")
+  .append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+  .append("g")
+    .attr("transform",
+          "translate(" + margin.left + "," + margin.top + ")");
 
-const thead = table.append("thead");
-const tbody = table.append("tbody");
+//Read the data
+d3.csv("/data/xmr-earnings.csv",
 
-thead.append("tr")
-  .selectAll("th")
-  .data(data[0]) // Assuming the first row contains the headers
-  .enter()
-  .append("th")
-  .text(columnName => columnName);
+  // When reading the csv, I must format variables:
+  function(d){
+    return { date : d3.timeParse("%Y-%m-%d")(d.date), value : d.value }
+  },
 
-tbody.selectAll("tr")
-  .data(data.slice(1)) // Slice the data to skip the first row (headers)
-  .enter()
-  .append("tr")
-  .selectAll("td")
-  .data(row => Object.values(row))
-  .enter()
-  .append("td")
-  .text(value => value);
+  // Now I can use this dataset:
+  function(data) {
 
-});
+    // Add X axis --> it is a date format
+    var x = d3.scaleTime()
+      .domain(d3.extent(data, function(d) { return d.date; }))
+      .range([ 0, width ]);
+    svg.append("g")
+      .attr("transform", "translate(0," + height + ")")
+      .call(d3.axisBottom(x));
+
+    // Add Y axis
+    var y = d3.scaleLinear()
+      .domain([0, d3.max(data, function(d) { return +d.value; })])
+      .range([ height, 0 ]);
+    svg.append("g")
+      .call(d3.axisLeft(y));
+
+    // Add the line
+    svg.append("path")
+      .datum(data)
+      .attr("fill", "none")
+      .attr("stroke", "steelblue")
+      .attr("stroke-width", 1.5)
+      .attr("d", d3.line()
+        .x(function(d) { return x(d.date) })
+        .y(function(d) { return y(d.value) })
+        )
+
+})
