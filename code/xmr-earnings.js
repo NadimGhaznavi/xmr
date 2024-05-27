0f6af4c0c9b8fddc28460cb659c64d88bce29150
NadimGhaznavi <nghaznavi@gmail.com>
2024-05-27 10:03:36 -0400

    d3 + apex

--- code/xmr-earnings.js
+++ code/xmr-earnings.js
@@ -1,52 +1,47 @@
-// set the dimensions and margins of the graph
-var margin = {top: 10, right: 30, bottom: 30, left: 60},
-    width = 460 - margin.left - margin.right,
-    height = 400 - margin.top - margin.bottom;
+d3.csv("/data/xmr-earnings.csv", function(data) {
+  // Convert data to X and R arrays for ApexCharts
+const xData = data.map(d => d.xValue); // Replace 'xValue' with your X data field name
+const rData = [];
+  for (let i = 0; i < data.length - 1; i++) {
+    rData.push(data[i + 1].xValue - data[i].xValue);
+  }
 
-// append the svg object to the body of the page
-var svg = d3.select("#csv-chart")
-  .append("svg")
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom)
-  .append("g")
-    .attr("transform",
-          "translate(" + margin.left + "," + margin.top + ")");
+  // Now create the ApexCharts XmR chart
+let chart = new ApexCharts(document.querySelector("#chart"), {
+    series: [{
+      name: 'X',
+      data: xData
+    }, {
+      name: 'R',
+      data: rData
+    }],
+    chart: {
+      type: 'line',
+      height: 350
+},
+    plotOptions: {
+      line: {
+        color: ['#FF6600', '#00796B']
+      }
+    },
+    dataLabels: {
+      enabled: true,
+      color: '#000000'
+},
+    stroke: {
+      width: [2, 2],
+      curve: 'smooth'
+},
+    fill: {
+      opacity: [0, 0]
+    },
+    markers: {
+      size: 0
+},
+    xaxis: {
+      type: 'datetime'
+}
+  });
 
-//Read the data
-//d3.csv("/data/xmr-earnings.csv",
-d3.csv("/data/xmr-earnings.csv").then(data => {
-
-  // When reading the csv, I must format variables:
-  function(d){
-    return { date : d3.timeParse("%Y-%m-%d")(d.date), value : d.value }
-  },
-
-  // Now I can use this dataset:
-  function(data) {
-
-    // Add X axis --> it is a date format
-    var x = d3.scaleTime()
-      .domain(d3.extent(data, function(d) { return d.date; }))
-      .range([ 0, width ]);
-    svg.append("g")
-      .attr("transform", "translate(0," + height + ")")
-      .call(d3.axisBottom(x));
-
-    // Add Y axis
-    var y = d3.scaleLinear()
-      .domain([0, d3.max(data, function(d) { return +d.value; })])
-      .range([ height, 0 ]);
-    svg.append("g")
-      .call(d3.axisLeft(y));
-
-    // Add the line
-    svg.append("path")
-      .datum(data)
-      .attr("fill", "none")
-      .attr("stroke", "steelblue")
-      .attr("stroke-width", 1.5)
-      .attr("d", d3.line()
-        .x(function(d) { return x(d.date) })
-        .y(function(d) { return y(d.value) })
-        )
-}})
+  chart.render();
+});
