5f23ff7b35f80e13181055d36fa6668d3a46fa33
NadimGhaznavi <nghaznavi@gmail.com>
2024-05-27 17:54:10 -0400

    syntax

--- _posts/2024-05-26-XMR-Earnings.md
+++ _posts/2024-05-26-XMR-Earnings.md
@@ -10,23 +10,9 @@ date: 2024-05-26
 <script src="https://d3js.org/d3.v4.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/apexcharts"></script>
 <script src="/assets/js/xmr-earnings.js"></script>
-<script src="/assets/js/test-chart.js"></script>
 
-<div id="wrapper">
-  <div id="areaChart">
+<p>
+  <div id="chart"></div>
+</p>
 
-  </div>
-  <div id="barChart">
-
-  </div>
-</div>
-
-<div id="wrapper">
-  <h1>foo</h1>
-  <div id="chart-area">
-
-  </div>
-  <div id="chart-bar">
-
-  </div>
 
--- assets/js/xmr-earnings.js
+++ assets/js/xmr-earnings.js
@@ -1,70 +1,28 @@
 document.addEventListener('DOMContentLoaded', function(
 ) {
-  var areaOptions = {
+  var options = {
     chart: {
-      id: "areaChart",
-      type: 'area',
-      height: 500,
-      foreColor: "#ccc",
-      toolbar: {
-        autoSelected: "pan",
-        show: false
-      }
-    },
-    colors: ["#00baec"],
-    stroke: {
-      width: 3
-    },
-    grid: {
-      borderColor: "#555",
-      clipMarkers: false,
-      yaxis: {
-        lines: {
-          show: false
-        }
-      }
-    },
-    dataLabels: {
-      enabled: false
-    },
-    fill: {
-      gradient: {
-        enabled: true,
-        opacityFrom: 0.55,
-        opacityTo: 0
-      }
-    },
-    markers: {
-      size: 5,
-      colors: ["#000524"],
-      strokeColor: "#00baec",
-      strokeWidth: 3
-    },
-    series: [
-      {
-        name: 'Total XMR Earned',
-        data: [] // Initialize to an empty array
-      }
-    ],
-    tooltip: {
-      theme: "dark"
-    },
+      type: 'line'
+},
+    series: [{
+      name: 'Total',
+      data: []
+    }],
     xaxis: {
-      type: "datetime",
       categories: []
-    },
-    yaxis: {
-      min: 0,
-      tickAmount: 4
     }
-  };
+  }
+
+  var chart = new ApexCharts(document.querySelector("#chart"), options);
+
+  chart.render();
 
   d3.csv("/_data/xmr-earnings.csv", function(data) {
     const dateData = data.map(d => d.Date);
     const totalData = data.map(d => d.Total);
 
     chart.updateSeries([{
-      name: 'Total XMR Earned',
+      name: 'Total',
       data: totalData
     }]);
     chart.updateOptions({
@@ -73,61 +31,4 @@ document.addEventListener('DOMContentLoaded', function(
       }
     });
   });
-
-  var areaChart = new ApexCharts(document.querySelector("#areaChart"), areaOptions);
-
-  areaChart.render();
-
-var barOptions = {
-  chart: {
-    id: 'barChart',
-    height: 250,
-    type: 'bar',
-    foreColor: "#ccc",
-    brush: {
-      target: "areaChart",
-      enabled: true
-    },
-    selection: {
-      enabled: true,
-      fill: {
-        color: "#fff",
-        opacity: 0.4
-      },
-      xaxis: {
-        min: new Date("21 March 2024 00:00:00").getTime(),
-        max: new Date("27 May 2024 23:59:59").getTime()
-      }
-    }
-  },
-  colors: ["#ff0080"],
-  series: [
-    {
-      data: []
-    }
-  ],
-  stroke: {
-    width: 2
-  },
-  grid: {
-    borderColor: "#444"
-  },
-  markers: {
-    size: 0
-  },
-  xaxis: {
-    type: "datetime",
-    tooltip: {
-      enabled: false
-    }
-  },
-  yaxis: {
-    tickAmount: 2
-  }
-};
-
-var barChart = new ApexCharts(document.querySelector("#barChart"), barOptions);
-
-barChart.render()
-
 });
