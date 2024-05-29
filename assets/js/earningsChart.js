de86c7228b4885d18a09b9be8b68605f6b57175b
NadimGhaznavi <nghaznavi@gmail.com>
2024-05-29 04:05:24 -0400

    Chart

--- assets/js/earningsChart.js
+++ assets/js/earningsChart.js
@@ -1,59 +1,48 @@
-//document.addEventListener('DOMContentLoaded', function() {
-  const dateData = [];
-  const totalData = [];
+const csvUrl = '/data/xmr-earnings.csv';
+const dateData = [];
+const totalData = [];
 
- // const loadCSV = async () => {
- //   const response = await fetch("/data/xmr-earnings.csv");
-  //  const text = await response.text();
+const fetchAndParseCSV = async () => {
+  const response = await fetch(csvUrl);
+  const text = await response.text();
 
-  const response = fetch("/data/xmr-earnings.csv");
-  const text = response.text();
+  return Papa.parse(text, {
+    header: true,
+    delimiter: ","
+  });
+};
 
-    const parsedData = Papa.parse(text, {
-      header: true,
-      delimiter: ","
-    });
+const loadCSV = async () => {
+  const parsedData = await fetchAndParseCSV();
 
-    parsedData.data.forEach(data => {
-      const date = data['Date'];
-      const total = data['Total'];
+  parsedData.data.forEach(data => {
+    const date = data['Date'];
+    const total = data['Total'];
 
-      dateData.push(date);
-      totalData.push(Number(total));
-    });
+    dateData.push(date);
+    totalData.push(Number(total));
+  });
 
-    const options = {
-      chart: {
-        height: 280,
-        type: "area"
-      },
-      dataLabels: {
-        enabled: false
-      },
-      series: [{
-        name: 'Total',
-        data: totalData
-      }],
-      xaxis: {
-        categories: dateData
-      },
-      stroke: {
-        width: 2
-      },
-      fill: {
-        type: "gradient",
-        gradient: {
-          shadeIntensity: 1,
-          opacityFrom: 0.7,
-          opacityTo: 0.9,
-          stops: [0, 90, 100]
-        }
-      }
-    };
+  const options = {
+    chart: {
+      height: 280,
+      type: "area"
+    },
+    dataLabels: {
+      enabled: false
+    },
+    series: [{
+      name: 'Total',
+      data: totalData
+    }],
+    xaxis: {
+      categories: dateData
+    },
+  };
 
-    var areaChart = new ApexCharts(document.querySelector("#areaChart"), options);
-    areaChart.render();
-  //};
-  //loadCSV();
+  var areaChart = new ApexCharts(document.querySelector("#areaChart"), options);
+  areaChart.render();
+
+};
   
-//});
+loadCSV();
