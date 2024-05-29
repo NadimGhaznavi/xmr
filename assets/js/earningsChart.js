25a58b8928446fbc8df801a6e128fbe67bca8c98
NadimGhaznavi <nghaznavi@gmail.com>
2024-05-29 00:16:20 -0400

    Added console.log() statements

--- assets/js/earningsChart.js
+++ assets/js/earningsChart.js
@@ -3,12 +3,14 @@ document.addEventListener('DOMContentLoaded', function() {
   const dateData = [];
   const totalData = [];
 
-
   d3.csv("/data/xmr-earnings.csv", function(csvData) {
     const { Date, Total } = csvData;
 
     dateData.push(Date);
     totalData.push(Number(Total));
+
+    console.log("dateData: ", dateData);
+    console.log("totalData: ", totalData);
 
     const options = {
       chart: {
@@ -26,7 +28,7 @@ document.addEventListener('DOMContentLoaded', function() {
         categories: dateData
       }
     };
-    
+
     var areaChart = new ApexCharts(document.querySelector("#areaChart"), options);
     areaChart.render();  
   });  
