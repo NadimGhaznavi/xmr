7aca2b3851b757e9212953fef23687c45288a075
Nadim Ghaznavi <nghaznavi@gmail.com>
2025-06-14 21:56:03 -0400

    New db4e reports

--- js/payments/cumulative-payment-180days.js
+++ js/payments/cumulative-payment-180days.js
@@ -1,3 +1,32 @@
+/* 
+tmpl/js/cumulative-payments.js
+
+This template is used by the Db4eReports module to generate report
+specific JavaScript files.
+*/
+
+
+/*
+  This file is part of *db4e*, the *Database 4 Everything* project
+  <https://github.com/NadimGhaznavi/db4e>, developed independently
+  by Nadim-Daniel Ghaznavi. Copyright (c) 2024-2025 NadimGhaznavi
+  <https://github.com/NadimGhaznavi/db4e>.
+ 
+  This program is free software: you can redistribute it and/or 
+  modify it under the terms of the GNU General Public License as 
+  published by the Free Software Foundation, version 3.
+ 
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+  General Public License for more details.
+
+  You should have received a copy (LICENSE.txt) of the GNU General 
+  Public License along with this program. If not, see 
+  <http://www.gnu.org/licenses/>.
+*/
+
+
 const csvUrl = '/csv/payments/cumulative-payment-180days.csv';
 const dateData = [];
 const totalData = [];
